9921ad43287d3a0d9ab5f6ca7b6009a1854c383c
Hugo Cabrita <ha.cabrita@campus.fct.unl.pt>
2015-08-10 14:24:08 +0100

    adding spectrwm again...

---
+++ spectrwm/autostart.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+feh --bg-scale /mnt/Data/Wallpapers/v3/fmd3Lk1.jpg &
+ibus-daemon -x -d -s &
+#uppr right corner, desactivate, left, activate
+xautolock -time 5 -locker 'i3lock -c 111111' -corners 0-+0  -cornerdelay 4 -cornerredelay 4 &
+$HOME/.scripts/touchpad_toggle &
+/usr/bin/start-pulseaudio-x11 &
+thunderbird &
+/usr/lib/lxpolkit/lxpolkit &
+$HOME/.scripts/drop_start &
+compton &
+urxvtd &
+#tint2 -c $HOME/.dotcats/spectrwm/tint2config &
+pcmanfm -d
