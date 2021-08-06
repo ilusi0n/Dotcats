f7510ca4bdcedc938e73c2c19cb5bf5ebe1029ef
ilusi0n <ha.cabrita@gmail.com>
2021-08-06 14:24:32 +0100

    update bspwm and polybar

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -3,5 +3,5 @@
 killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar -r -c ~/.config/polybar/config.ini main &
-polybar -r -c ~/.config/polybar/config.ini main2 &
+polybar -c ~/.config/polybar/config.ini main &
+polybar -c ~/.config/polybar/config.ini main2 &
